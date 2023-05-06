e4aea5de5bafd40b954a4355b661af0d59636f86
medina10-01 <mstiven748@gmail.com>
2023-05-06 14:41:40 -0500

    hola

--- js/lotus.js
+++ js/lotus.js
@@ -3,11 +3,11 @@ let before = document.querySelector('#btnBefore');
 let imgCont = document.querySelector('#imgCont');
 
 function imgNex() {
-    imgCont.setAttribute("src", ("img/lotus (2).webp"));
+    imgCont.setAttribute("src", ("/img/lotus (2).webp"));
 }
 
 function imgBefore() {
-    imgCont.setAttribute("src", ("img/lotus (1).webp"));
+    imgCont.setAttribute("src", ("/img/lotus (1).webp"));
 }
 
 
